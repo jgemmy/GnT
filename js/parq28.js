a9990f3ebcb3fc93c30594b0f96382007599421f
jgemmy <jgemmy90@gmail.com>
2014-10-21 03:42:25 +0200

    Commit-2
    Backup 21.10

---
+++ js/parq28.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+        
+       $.ajax({
+            method: 'GET',
+            url: 'js/db/im28parq.json',
+            dataType: "json",
+            success: function (data) {
+                
+                for (var i=0; i<data.length; i++) {
+                        $('<tr>').append(
+                        $('<td>').text(data[i].idp),
+                        $('<td>').text(data[i].dim),
+                        $('<td>').text(data[i].qual),
+                        $('<td>').text(data[i].desc),
+                        $('<td>').text(data[i].prezzo)).appendTo('#records_table');
+                    }
+            },
+            error: function (a,b,c) {
+                alert("Error");
+            }
+        });
+    });
