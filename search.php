6987991cb8e0444dc717c237e306fe81cfba749a
jgemmy <jgemmy90@gmail.com>
2014-11-20 00:59:25 +0100

    commit-7
    Backup 20.11

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Search results</title>
+    <title>Risultati Della Ricerca </title>
     <meta charset="utf-8">  
     <link rel="icon" href="images/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
@@ -44,22 +44,68 @@
           <div class="span12">
         <div class="navbar navbar_">
               <div class="container">
-            <h1 class="brand brand_"><a href="index.html"><img alt="" src="img/logo.gif"> </a></h1>
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse_">Menu <span class="icon-bar"></span> </a>
+            <!--<h1 class="brand brand_"><a href="index.html"><img alt="" src="img/add.png"> </a></h1> -->
+            <a class="btn btn-navbar btn-navbar_" data-toggle="collapse" data-target=".nav-collapse_">Menu <span class="icon-bar"></span> </a>
             <div class="nav-collapse nav-collapse_  collapse">
                   <ul class="nav sf-menu">
-                <li><a href="index.html">About</a></li>
-                <li class="sub-menu"><a href="index-1.html">Services</a>
-                   <ul>
-                    <li><a href="#">Dolore </a></li>
-                    <li><a href="#">Consecte</a></li>
-                    <li><a href="#">Conseq</a></li>
+                <li ><a href="index.html">Home</a></li>
+                <li class="sub-menu"><a href="persiane.html">Persiane</a>
+                      <ul>
+                    <li><a href="persianaClasse2.html">Classe 2</a></li><hr>  
+                    <li><a href="persianeclasse3.html">Classe 3</a></li><hr>  
+                    <li><a href="persianeorientabili3.html">Orientabile Classe 3</a></li>  
+                    
+                  </ul>
+                    </li>
+
+                <li class="sub-menu"><a href="grate.html">Grate</a>
+                      <ul>
+                    <li><a href="gratacombinato.html">Combinato</a></li><hr>
+                    <li><a href="gratasnodata.html">Snodata</a></li><hr>  
+                    <li><a href="gratafissa.html">Fissa / Apribile</a></li>  
+                    </ul>
+                    </li>
+
+                 <li class="sub-menu"><a href="infissi.html">Infissi</a>
+                      <ul>
+                    <li><a href="infissi.html#omeca">Omeca</a></li><hr>  
+                    <li><a href="infissi.html#arcer">Arcer</a></li><hr>  
+                    <li><a href="infissi.html#rehau">Rehau</a></li>
+                  </ul>
+                  </li>
+                  
+                  <li class="sub-menu"><a href="termocamini.html">Termo <br>Camini </a>
+                      <ul>
+                    <li><a href="termoelite.html">Elite</a></li><hr>
+                    <li><a href="termoprestige.html">Prestige</a></li><hr>  
+                    <li><a href="termorefrattario.html">Refrattario</a></li><hr>  
+                    <li><a href="termoregal.html">Regal</a></li><hr>
+                    <li><a href="termoregalturbo.html">Regal Turbo</a></li>
+                  </ul>
+                  </li>
+
+                <li  class="sub-menu"><a href="porteinterno.html">Porte Da Interno</a>
+                <ul>
+                    <li><a href="porteinterno.html#mass">Massello</a></li><hr>
+                    <li><a href="portetechwood.html">Techwood</a></li><hr>  
+                    <li><a href="portetelist.html">Telist</a></li><hr>  
+                    <li><a href="portetrend.html">Trend</a></li>
+                  </ul>
+                  </li>
+          
+                <li class="sub-menu"><a href="porteblindate.html">Porte Blindate</a>
+                <ul>
+                   <li><a href="termoelite.html">Standard</a></li><hr>
+                    <li><a href="termoprestige.html">Personalizzata</a></li><hr>  
+                    <li><a href="termorefrattario.html">Pannelli Rivestimento</a></li>   
                   </ul>
                 </li>
-                <li><a href="index-2.html">Folio</a></li>
-                <li><a href="index-3.html">Blog</a></li>
-                <li><a href="index-4.html">Contact</a></li>
+                <li><a href="controtelai.html">Controtelai </a></li>
+                <li><a href="parquet.html">Parquet </a></li>
+              
               </ul>
+
+            
                 </div>
           </div>
             </div>
@@ -74,7 +120,7 @@
   <div class="container">
     <div class="row">
     	<div class="span12">
-            <h3>Search result:</h3>
+            <h3>Risultati della ricerca:</h3>
             <div id="search-results"></div>
 		</div>  
     </div>        
@@ -84,16 +130,21 @@
 </div>
 <!--============================== footer =================================-->
 <footer>
+<div style="text-align: center; margin: 0px; padding: 0px; list-style-type:">
+  <p style="color: black;"><a class="fotmenu"  href="index.html">Home</a> | <a class="fotmenu" href="persiane.html">Persiane</a> | <a class="fotmenu" href="grate.html">Grate</a> | <a class="fotmenu" href="infissi.html">Infissi</a> | <a class="fotmenu" href="caselegno.html">Case In Legno</a> | <a class="fotmenu" href="termocamini.html">Termocamini</a> | <a class="fotmenu" href="porteinterno.html">Porte Da Interno</a> | <a class="fotmenu" href="metalmeccanica.html">Metalmeccanica</a> | <a class="fotmenu" href="porteblindate.html">Porte Blindate</a> | <a class="fotmenu" href="controtelai.html">Controtelai</a> | <a class="fotmenu"  href="parquet.html">Parquet</a> </p>
+
+  </div><br><br>
+    </footer>
       <div class="container clearfix">
     <ul class="list-social pull-right">
           <li><a class="icon-1" href="#"></a></li>
           <li><a class="icon-2" href="#"></a></li>
           <li><a class="icon-3" href="#"></a></li>
           <li><a class="icon-4" href="#"></a></li>
+          <li><a class="icon-5" href="#"></a></li>
         </ul>
-    <div class="privacy pull-left">Website Template designed by <a href="http://www.templatemonster.com " target="_blank">TemplateMonster.com </a> </div>
+    <!--<div class="privacy pull-left">Website Template designed by <a href="http://www.templatemonster.com/" target="_blank" rel="nofollow">TemplateMonster.com</a> </div> -->
   </div>
-    </footer>
 <script type="text/javascript" src="js/bootstrap.js"></script>
 </body>
 </html>
